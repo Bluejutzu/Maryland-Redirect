77358d5c90f1f111aa7de2e7acdc60cc23ba59f8
Bluejutzu <kalebdaniel98@gmail.com>
2024-01-13 21:08:15 +0100

    Initial react project

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+/** @format */
+
+import "./App.css";
+import SlideInNotifications from "./SlideInNotifications.jsx";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Click the button!</h1>
+        <SlideInNotifications />
+      </header>
+    </div>
+  );
+}
+
+export default App;
